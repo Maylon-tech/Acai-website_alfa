8c70549746fba35125e4bb3d3b912bbf4cf77ffa
Maylon-tech <minatokiroisen77@gmail.com>
2023-04-20 22:40:59 +0900

    setting up the project ecommerce

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-component'
+
+const Container = styled.div``
+
+const Navbar = () => {
+  return (
+    <Container>
+        Navbar here
+    </Container>
+  )
+}
+
+export default Navbar
