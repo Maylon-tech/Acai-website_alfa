03764ba914697700f80b61825f695aa64e38b439
Maylon-tech <minatokiroisen77@gmail.com>
2023-05-05 23:35:54 +0900

    creating Hero section insted of Slider

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './style'
+const Hero = () => {
+  return (
+    <Container>
+        <h1>Hero section main of Home page</h1>
+    </Container>
+  )
+}
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
-import styled from 'styled-components'
 import Announcement from '../components/Announcement/Announcement'
-import Slider from '../components/Slider/Slider';
-
-export const Container = styled.div`
-  
-`;
-
-
+import Hero from '../components/Hero/Hero'
 
 const Home = () => {
   return (
     <div>
       <Announcement />
         <Navbar />
-        <Slider />
+        <Hero />
     </div>
   )
 }
